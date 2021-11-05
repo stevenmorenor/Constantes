430a924c28184f96a0b816707f779d09a6018a6d
steven <stevenmoreno498@gmail.com>
2021-11-04 19:43:39 -0500

    Se agrega proyecto

---
+++ Constantes/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constantes
+{
+    public enum Days
+    {
+        Monday, Thuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+    public class Demo
+    {
+        public const decimal Gravity = 9.8m;
+        public const string Text = "HELLOW THIS IS MY FRIST EXAMPLE";
+        public const int Days = 7;
+    }
+}
---
+++ Exampol/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exampol
+{
+    internal class Program
+    {
+        public const decimal Pi = 3.1415m;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Constantes.Demo.Text);
+            decimal G = Constantes.Demo.Gravity;
+            Console.WriteLine("The gravity is {0}", G);
+            int Week = Constantes.Demo.Days;
+            Console.WriteLine("The days of week are: {0}", Week);
+
+            foreach (var day in System.Enum.GetValues(typeof(Constantes.Days)))
+            {
+                Console.WriteLine(day);
+            }
+            decimal x = Pi;
+            Console.WriteLine(x);
+        }
+    }
+}
